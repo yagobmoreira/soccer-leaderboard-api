e563d4149d699907a7a40b26f8b270638422b716
Yago Moreira <yagomoreira@gmail.com>
2024-01-30 19:51:55 -0300

    Update Validations

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -1,14 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import schemas from './validations/schemas';
 
+const verifyErrorType = (type: string): number => {
+  if (type === 'string.email' || type === 'string.min') {
+    return 401;
+  }
+  return 400;
+};
+
 class Validations {
   static validateLogin(req: Request, res: Response, next: NextFunction) {
     const data = req.body;
     const { error } = schemas.loginSchema.validate(data);
-    console.log(error);
 
     if (error) {
-      return res.status(400).json({ message: error.message });
+      console.log(error);
+      const errorStatus = verifyErrorType(error.details[0].type);
+      return res.status(errorStatus).json({ message: error.message });
     }
 
     next();
--- app/backend/src/middlewares/validations/schemas.ts
+++ app/backend/src/middlewares/validations/schemas.ts
@@ -6,10 +6,12 @@ const loginSchema = Joi.object({
   email: Joi.string().email().required().messages({
     'any.required': filled,
     'string.empty': filled,
+    'string.email': 'Invalid email or password',
   }),
   password: Joi.string().min(7).required().messages({
     'any.required': filled,
     'string.empty': filled,
+    'string.min': 'Invalid email or password',
   }),
 });
 
