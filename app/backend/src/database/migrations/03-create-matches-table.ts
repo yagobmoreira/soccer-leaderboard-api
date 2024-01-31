f407692456eeb51bf815fe11fc3fdc4f0e9a5071
Yago Moreira <yagomoreira@gmail.com>
2024-01-31 08:23:27 -0300

    Interface IModel. Migration create-matches-table

---
+++ app/backend/src/database/migrations/03-create-matches-table.ts
@@ -0,0 +1,55 @@
+import { QueryInterface, Model, DataTypes } from "sequelize";
+import { IMatch } from "../../Interfaces/matches/IMatch";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IMatch>>('matches', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false 
+      },
+      homeTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "home_team_id",
+        references: {
+          model: {
+            tableName: "teams"
+          },
+          key: "id"
+        }
+      },
+      homeTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "home_team_goals" 
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "away_team_id",
+        references: {
+          model: {
+            tableName: "teams"
+          },
+          key: "id"
+        }
+      },
+      awayTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "away_team_goals" 
+      },
+      inProgress: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: "in_progress"
+      }
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('matches')
+  }
+}
